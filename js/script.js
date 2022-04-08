c64267607534f0e929e345f26045584f7bf24c54
Marco-Gagliano <brazil_94@hotmail.it>
2022-04-08 17:50:33 +0200

    scrittura-procedimento

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* PROCEDIMENTO DELL'ESERCIZIO
+
+  1. Scrivere una linea di codice che visualizzi sulla pagina numeri da 1 a 100 dentro un container con le dovute specifiche
+  2. Per i multipli di 3 stampare Fizz,
+  3. Per i multipli di 5 stampare Buzz,
+  . Per i multi di 3 e 5 stampare FizzBuzz,
+  . Creare stili differenti per le 3 situazioni sovrastanti.
+  . Dichiarare le variabili,
+  . Creare un ciclo For con start 1 e limite 100,
+  . Aggiungere le condizioni IF per i multipli con aggiunta classi,
+  . Scrivere dentro il container nel DOM i vari div.
+*/

943823d536e336b30dcc9d6dc7a9256412e8d833
Marco-Gagliano <brazil_94@hotmail.it>
2022-04-08 17:51:27 +0200

    dichiarazione-valori-e-specifiche-di-calcolo

--- js/script.js
+++ js/script.js
@@ -10,3 +10,18 @@
   . Aggiungere le condizioni IF per i multipli con aggiunta classi,
   . Scrivere dentro il container nel DOM i vari div.
 */
+
+
+
+// salvare il container (o qualsiasi classe)
+const container = document.querySelector ('.container');
+console.log(container);
+
+// dichiarare dei valori o altro che a noi interessa affinchè possano servirci per poterli poi visualizzare
+const limitCounter = 101
+const boxThree = 3 
+const boxFive = 5
+const boxFinal = 101
+
+//specifica di calcolo
+for (let i = 1; i <= limitCounter; i++){
